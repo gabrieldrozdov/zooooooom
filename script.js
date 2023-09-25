ab9c4c4a5469be14268efeb530c8a4b918b9b4ed
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2023-09-25 16:26:18 -0400

    ppi calculation

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-// MY COMPUTER SPECS:
-// width: 1728px
-// height: 1117px
-// real diagonal: 16.2in
-// ppi: 127.01
+// Code adapted from https://www.ginifab.com/feeds/cm_to_inch/actual_size_ruler.html
 const r = document.querySelector(':root');
-const myRatio = 1728/1117;
-const myDPI = 127.01;
-const userRatio = screen.width/screen.height;
-const diffRatio = myRatio - userRatio;
-// const adjustedDPI = myDPI*(1+diffRatio);
-const adjustedDPI = myDPI;
-r.style.setProperty('--ppi', adjustedDPI + "px");
+function calculatePPI(){
+	let iHeight=screen.height, iWidth=screen.width;
+	if (navigator.userAgent.match(/iPhone/i)) { // iPhone
+		if (iWidth === 375 && iHeight === 667){ // iPhone 6,7,8
+			return 163;
+		} else if (iWidth === 375 && iHeight === 812) { // iPhone X,XS,11 Pro
+			return 152.7;
+		} else if (iWidth === 414 && iHeight === 896) { // iPhone 11
+			return 163;
+		} else if (iWidth === 390 && iHeight === 844) { // iPhone 12, 13, 14
+			return 153.3;
+		} else if (iWidth === 428 && iHeight === 926) { // iPhone 12 Pro Max, 13 Pro Max, 14 Plus
+			return 152.7;
+		} else if (iWidth === 430 && iHeight === 932) { // iPhone 14 Pro Max
+			return 153.3;
+		} else if (iWidth === 393 && iHeight === 852) { // iPhone 14 Pro
+			return 153.3;
+		} else { // iPhone 4/4s, iPhone 6+/6s+/7+/8+
+			return 163;
+		}
+	} else if (navigator.userAgent.match(/Macintosh/i)) { // Macbook air
+		if (iWidth === 810 && iHeight === 1080) { // iPad 7/8/9 gen
+			return 132;  
+		} else {
+			return 127.7;
+		}
+	} else if (navigator.userAgent.match(/Android.*Pixel/i)) { // Google Pixel
+		if (navigator.userAgent.match(/Pixel 5/i)){
+			return 157;
+		} else if (navigator.userAgent.match(/Pixel 4 XL/i)){
+			return 153;
+		} else if (navigator.userAgent.match(/Pixel 4/i)){
+			return 161;
+		} else {
+			return 160;
+		}
+	} else if (navigator.userAgent.match(/Android.*SAMSUNG/i)) { // Samsung
+		return 141;
+	} else if (screen.width<500 || screen.height<500) {
+		return 122.6;
+	} else {
+		if (screen.width === 1440 || screen.height === 900) {
+			return 89.4;
+		} else {
+			return 127.01;
+		}
+	}
+}
+let ppi = calculatePPI();
+r.style.setProperty('--ppi', ppi + "px");
 
 // Generate ruler
 let ruler = document.querySelector('.ruler');
@@ -81,8 +120,8 @@ function startGame() {
 		posX += increment;
 		ruler.style.transform = `translateX(-${posX}px)`;
 
-		if (posX >= adjustedDPI*200) {
-			ruler.style.transform = `translateX(-${adjustedDPI*200-1}px)`;
+		if (posX >= ppi*200) {
+			ruler.style.transform = `translateX(-${ppi*200-1}px)`;
 			message.innerText = "You missed it!";
 			endGame();
 		}
@@ -92,8 +131,8 @@ function startGame() {
 	ruler.dataset.start = 1;
 }
 function endGame() {
-	stop.innerHTML = (posX/adjustedDPI).toFixed(2) + "&quot;";
-	score.innerHTML = Math.abs(120 - posX/adjustedDPI).toFixed(2) + "&quot;";
+	stop.innerHTML = (posX/ppi).toFixed(2) + "in";
+	score.innerHTML = Math.abs(120 - posX/ppi).toFixed(2) + "in";
 	outro.style.display = "flex";
 	game = false;
 	ruler.dataset.start = 0;
